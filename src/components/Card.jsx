ee2358d4ffdd1425b0f9b68971ca3b6208b88d98
Aaron Zheng <153031902+aaronxyzheng@users.noreply.github.com>
2026-03-01 09:36:58 -0500

    style: add tilt to all cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,12 @@
+import Tilt from "react-parallax-tilt";
+
 function Card({ url, onClick }) {
     return (
-        <div onClick={() => onClick(url)}>
-            <img src={url} />
-        </div>
+        <Tilt tiltMaxAngleX={15} tiltReverse={true} tiltMaxAngleY={15} scale={1.05}>
+            <div onClick={() => onClick(url)}>
+                <img src={url} />
+            </div>
+        </Tilt>
     );
 }
 
